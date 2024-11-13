d71a6a3bbc8c2da270b46a364cf6bfc7d4b80f49
Egor-Frolov <zloisnegov@gmail.com>
2024-11-14 00:43:39 +0300

    Add files via upload

---
+++ module_1_3..py
@@ -0,0 +1,11 @@
+# создайте переменную name и присвойте ей тип данных str
+name = 'Egor'
+print ('Name:',name, type (name))
+# создайте переменную age и присвойте ей тип данных int
+age = 28
+print ('Age:',age, type (age))
+# перепишите age  с новым значением
+print ('New Age:',float(age)+0.5, type (float(age)+0.5))
+# создайте переменную is_student с типом данных bool
+is_student = True
+print ('Is Student:',is_student, type (is_student))
